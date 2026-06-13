9db2674fa15fc38d3eabebd6e6d16a76f81cc089
Ritvik2013 <Rathidevisuresh6@gmail.com>
2026-06-12 23:26:18 -0600

    Add files via upload

---
+++ class3.py
@@ -0,0 +1,11 @@
+#creating variable#
+m="My mom's birtday is on May 3"
+d="My dad's birthday is on June 6"
+u="My uncle's birthday is on Jan 2"
+a="My auntie birthday is on September 21"
+f="My friend birthday is on March 19"
+print(m)
+print(d)
+print(u)
+print(a)
+print(f)
